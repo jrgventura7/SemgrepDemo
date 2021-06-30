eff724f9a4e5760f0251c013afe6c7a17be4852c
Jorge Ventura <jorge.venturag@outlook.com>
2021-06-29 22:27:06 -0500

    (feat): second example

---
+++ src/servlet/Semgrep03Servlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Semgrep03Servlet {
+
+	Connection cn = null;
+	
+	private boolean search(String search) {
+		
+		String query01 = "SELECT id, name, editorial, year FROM books WHERE name = " + search;  
+		
+		try {
+            PreparedStatement ps = cn.prepareStatement(query01);
+            ps.setString(1,search);
+            
+        } catch (SQLException e) {
+            
+        }
+		
+		return true;
+		
+	}
+	
+	private boolean searchSecond(String search) {
+		
+		String query01 = "SELECT id, name, editorial, year FROM books WHERE name = " + search + " limit 1";  
+		
+		try {
+            PreparedStatement ps = cn.prepareStatement(query01);
+            ps.setString(1,search);
+            
+        } catch (SQLException e) {
+            
+        }
+		
+		return true;
+		
+	}
+	
+	private boolean searchCorrect(String search) {
+				
+		String query01 = "SELECT id, name, editorial, year FROM books WHERE name = ? limit 1";  
+		
+		try {
+            PreparedStatement ps = cn.prepareStatement(query01);
+            ps.setString(1,search);
+            
+        } catch (SQLException e) {
+            
+        }
+		
+		return true;
+		
+	}
+	
+}
